5fedaf8c04778d268fd69ebc4d7c8c70902160a2
Leonardo <leomatorres1901@gmail.com>
2026-06-10 20:18:13 -0300

    Update lista-ox.c

--- lista-ox.c
+++ lista-ox.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include "function_lista.c"
 
-int main(int argc, char *argv[])
- {
-    char lista[9] = {0}; 
+int main(int argc, char *argv[]) 
+{
+    char lista[9] = {0};
 
-    
-    if (argc > 1) 
-    {
-        preencher_argumentos(lista, argc, argv);
+
+    if (argc > 1)
+     {
+        preencher_por_argumentos(lista, argc, argv);
     } 
     else 
     {
-        
+      
         for (int i = 0; i < 9; i++) 
         {
             char temp;
             printf("Digite o caractere %d da lista: ", i + 1);
-            scanf(" %c", &temp); 
+            scanf(" %c", &temp);
 
-            while (!validar_caractere(temp))
-             {
+            while (!validar_caractere(temp)) 
+            {
                 printf("caractere invalido\n");
                 printf("Digite o caractere %d da lista: ", i + 1);
                 scanf(" %c", &temp);
@@ -29,10 +29,13 @@ int main(int argc, char *argv[])
             lista[i] = temp;
         }
     }
+
+   
     for (int i = 0; i < 9; i++) 
     {
         printf("Caractere %d: %c\n", i + 1, lista[i]);
     }
+
     printf("\nFeito por Leonardo Mattoso (https://github.com/Leonardo-Torres01)");
     return 0;
 }
